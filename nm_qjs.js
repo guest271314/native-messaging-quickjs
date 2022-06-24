3fad7101f66063554040ea7621a180b0b7e80a74
guest271314 <guest271314@gmail.com>
2022-06-23 19:41:43 -0700

    Update nm_qjs.js

--- nm_qjs.js
+++ nm_qjs.js
@@ -3,25 +3,19 @@
 // guest271314, 6-19-2022
 import * as std from 'std';
 
+const length = Uint32Array.BYTES_PER_ELEMENT;
+
 function getMessage() {
   const header = new Uint32Array(1);
-  std.in.read(header.buffer, 0, header.byteLength);
-  const length = header[0];
-  const output = new Uint8Array(length);
-  std.in.read(output.buffer, 0, length);
+  std.in.read(header.buffer, 0, length);
+  const output = new Uint8Array(header[0]);
+  std.in.read(output.buffer, 0, output.length);
   return output;
 }
 
 function sendMessage(json) {
-  const header = new Uint8Array(
-    Uint32Array.from(
-      {
-        length: 4,
-      },
-      (_, index) => (json.length >> (index * 8)) & 0xff
-    )
-  );
-  std.out.write(header.buffer, 0, header.length);
+  const header = new Uint32Array([json.length]);
+  std.out.write(header.buffer, 0, length);
   std.out.puts(json);
   std.out.flush();
   return true;
