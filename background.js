3f1d9e62eab17a6f79da91101da0afc6dd7b0f2a
guest271314 <guest271314@gmail.com>
2022-10-05 18:01:02 -0700

    Update background.js

--- background.js
+++ background.js
@@ -1,4 +1,4 @@
 var port = chrome.runtime.connectNative('nm_qjs');
 port.onMessage.addListener((message) => console.log(message));
 port.onDisconnect.addListener((e) => console.log(e));
-port.postMessage(new Array((200000)));
+port.postMessage(new Array(200000));
