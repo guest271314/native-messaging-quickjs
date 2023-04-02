8f1eed99945b2fc6ee5290dabef24de26224100b
guest271314 <guest271314@gmail.com>
2023-04-01 18:35:07 -0700

    Create webserver.js

---
+++ webserver.js
@@ -0,0 +1,11 @@
+import { webserver } from './webserver.so';
+try {
+  webserver('parec -d @DEFAULT_MONITOR@', (status) => {
+    console.log(status);
+    if (status === 'aborted') {
+      std.exit(0);
+    }
+  });
+} catch (e) {
+  console.log(e);
+}
